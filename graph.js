ea079cede9f4e0b7eb825372b5270b78248e44ce
Evan Hesketh <evanhesketh@yahoo.ca>
2023-06-01 20:14:31 -0400

    complete exercise

--- graph.js
+++ graph.js
@@ -128,63 +128,27 @@ class Graph {
     return visited;
    }
 
-  // start:
-  // end:
-
-
-  // call stack:
-
-
-  // distanceOfShortestPath(H, M) Adjacents:[M]  Neighbor:M  Seen:
-  // distanceOfShortestPath(R, M) Adjacents:[H, T, I]  Neighbor:H  Seen:
-
-
-
   /** find the distance of the shortest path from the start vertex to the end vertex */
 
   distanceOfShortestPath(start, end) {
-    let paths = 0;
+    // let level = 0;
+    const toVisitQueue = [[start, 0]];
+    const seen = new Set();
+    seen.add(start);
 
-    function innerFunction(start, end) {
-      if (start === end) return 0;
-      if (start.adjacent.has(end)) return 1;
+    while (toVisitQueue.length) {
+      const [current, myLevel] = toVisitQueue.shift();
 
-      let seen = new Set([start]);
+      if (current === end) return myLevel;
 
-      for (const neighbor of start.adjacent) {
+      for (let neighbor of current.adjacent) {
         if (!seen.has(neighbor)) {
+          toVisitQueue.push([neighbor, myLevel + 1]);
           seen.add(neighbor);
-          const temp = 1 + innerFunction(neighbor, end);
-          console.log("temp", temp);
-          paths = Math.min(temp, paths);
         }
       }
     }
-    innerFunction(start, end);
-    return paths;
-  }
-
-
-  // distanceOfShortestPath(start, end) {
-  //   if (start === end) return 0;
-  //   if (start.adjacent.has(end)) return 1;
-
-  //   let seen = new Set([start]);
-
-  //   let shortest = Infinity;
-  //   for (const neighbor of start.adjacent) {
-  //     if (!seen.has(neighbor)) {
-  //       seen.add(neighbor);
-  //       const temp = 1 + this.distanceOfShortestPath(neighbor, end)
-  //       shortest = Math.min(shortest, temp)
-  //     }
-  //   }
-  //  }
-
-  // distanceOfShortestPath(start, end) {
-
-  // }
-
+}
 }
 
 module.exports = { Graph, Node }
